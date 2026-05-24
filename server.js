96438e9ad9cd544aaa68a9682a43420c82a8b9c3
Erothu Yogeswari <yogeswarierothu123@gmail.com>
2026-05-24 18:57:45 +0530

    update

--- server.js
+++ server.js
@@ -44,6 +44,10 @@ app.use(generalLimiter)
 
 connectDB()
 initRedis()
+app.use((req, res, next) => {
+  console.log("Request:", req.method, req.url);
+  next();
+});
 
 // Socket.IO connection handling
 io.on("connection", (socket) => {
